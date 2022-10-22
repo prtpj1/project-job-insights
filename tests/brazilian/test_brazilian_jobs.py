ba404b7d9b4c1b2910bb1e2a5d5e2686f6c19129
prtpj1 <prtpj1@gmail.com>
2022-10-22 15:40:53 -0300

    :mask: tests:(Requisite 11) - method read_brazilian_file :arrow_right: test if the method has the keys correctly translated back to english

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+
+PATH = "tests/mocks/brazilians_jobs.csv"
+jobs_mock = {"title": "Maquinista", "salary": "2000", "type": "trainee"}
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file(PATH)
+    assert jobs_mock in jobs
